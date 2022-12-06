51938eee15a5be87d5156d86a58e091630097d95
alexsharich <alexsharich@gmail.com>
2022-12-06 01:37:24 +0100

    add cors middleware

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import express from 'express'
 import mongoose from 'mongoose'
-import {registerValidation} from './validation/auth.js'
+import cors from 'cors'
 
+import { registerValidation } from './validation/auth.js'
 import checkAuth from './utils/checkAuth.js'
 import * as UserController from './controllers/UserController.js'
 
@@ -12,13 +13,14 @@ mongoose.connect('mongodb+srv://alexandev444:s201290935s@cluster0.zuwj3nx.mongod
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
-app.post('/auth/register',registerValidation,UserController.register)
-app.post('/auth/login',UserController.login)
+app.post('/auth/register', registerValidation, UserController.register)
+app.post('/auth/login', UserController.login)
 app.get('/auth/me', checkAuth, UserController.getMe)
 app.listen(3001, (err) => {
-    if (err) {
-        return console.log(err)
-    }
-    console.log('Server OK')
+  if (err) {
+    return console.log(err)
+  }
+  console.log('Server OK')
 })
