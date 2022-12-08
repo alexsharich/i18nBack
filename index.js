d08b6b69f6c24876f8dc46902dfbaf9c28596254
alexsharich <alexsharich@gmail.com>
2022-12-08 16:49:32 +0100

    delete res.json({}) in userController.getme

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express'
 import mongoose from 'mongoose'
 import cors from 'cors'
 
-import { registerValidation } from './validation/auth.js'
+import {registerValidation} from './validation/auth.js'
 import checkAuth from './utils/checkAuth.js'
 import * as UserController from './controllers/UserController.js'
 
@@ -15,12 +15,12 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-app.post('/auth/register', registerValidation, UserController.register)
-app.post('/auth/login', UserController.login)
+app.post('/auth/register',registerValidation,UserController.register)
+app.post('/auth/login',UserController.login)
 app.get('/auth/me', checkAuth, UserController.getMe)
 app.listen(3001, (err) => {
-  if (err) {
-    return console.log(err)
-  }
-  console.log('Server OK')
+    if (err) {
+        return console.log(err)
+    }
+    console.log('Server OK')
 })
--- utils/checkAuth.js
+++ utils/checkAuth.js
@@ -1,22 +1,22 @@
 import jwt from 'jsonwebtoken'
 
-export default (req, res, next) => {
-  const token = (req.headers.authorization || '').replace(/Bearer\s?/, '')
-  if (token) {
-    try {
-      const decoded = jwt.verify(token, 'secretKEY')
+export default (req,res,next)=>{
+  const token = (req.headers.authorization || '').replace(/Bearer\s?/,'')
+  if(token){
+try{
+const decoded = jwt.verify(token,'secretKEY')
 
-      req.userId = decoded._id
+req.userId = decoded._id
 
-      next()
-    } catch (err) {
-      return res.status(403).json({
-        message: 'No access ...'
-      })
-    }
-  } else {
-    return res.status(403).json({
-      message: 'No access ...'
+next()
+}catch(err){
+  return res.status(403).json({
+    message:'No access ...'
+  })
+}
+  }else {
+   return res.status(403).json({
+      message:'No access ...'
     })
   }
 }
